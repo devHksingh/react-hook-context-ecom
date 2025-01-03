a8e6661d373b39b05beb6f6c3b6541784cfc1dbd
devHksingh <hksingh.dev@gmail.com>
2025-01-03 20:57:33 +0530

    fix add cart state

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -3,18 +3,6 @@ import getAllProducts from "../utils/fetchProducts"
 import { FiHeart, FiMinus, FiPlus, FiShoppingCart } from "react-icons/fi"
 
 const Products = () => {
-    const [products,setProducts]= useState([])
-    const [cartProduct,setCartProduct] = useState([])
-    const [quantity,setQuantity] = useState({productId:null,quantity:0})
-    useEffect(()=>{
-        async function getData(){
-            const fetchData = await getAllProducts()
-            setProducts(fetchData)
-
-        }
-        getData()
-        
-    }, [])
     interface Products{
         id:number
         category:string
@@ -28,7 +16,98 @@ const Products = () => {
         price:number
 
     }
+    // interface CartItem{
+    //     productId:number
+    //     quantity:number
+    // }
+    const [products,setProducts]= useState<Products[]>([])
+    const [cartProduct,setCartProduct] = useState([{productId:0,quantity:0}])
+    // const [quantity,setQuantity] = useState([])
+    
+    useEffect(()=>{
+        async function getData(){
+            const fetchData = await getAllProducts()
+            setProducts(fetchData)
 
+        }
+        getData()
+        
+    }, [])
+
+    // const handleQuantity =(id:number,action:"add"|"remove")=>{
+    //     console.log(action)
+               
+    //     setCartProduct((prev)=>{
+    //         const existingItem = prev.find((item)=> item.productId ===id)
+    //         if(existingItem){
+    //             return prev.map(item => item.productId === id?
+    //                 {...item,quantity:item.quantity+1}:item
+    //             )
+    //         }
+    //         return [...prev,{productId:id,quantity:1}]
+    //     })
+    // }
+    // const handleQuantity = (id:number,action:"add"|"remove")=>{
+    //     console.log(id,action)
+    //     setCartProduct((prev)=>{
+    //         const existingItem = prev.find((item)=>item.productId === id)
+    //         console.log(existingItem);
+    //         if(existingItem){
+    //             if(action === "add"){
+    //                 return prev.map((item)=>item.productId ===id?{...item,quantity:item.quantity+1}:item)
+    //             }else if(action === "remove"){ //remove product if its quantity is less then 1
+    //                 // return prev.map((item)=> {
+    //                 //     return item.quantity>1? prev.map((item)=>item.productId === id?{...item,quantity:item.quantity-1}:item):prev.filter((item)=>item.productId !== id)
+    //                 // })
+    //                 return existingItem.quantity>1 ? prev.
+    //             }
+    //         }else{
+    //             if(action === "add"){
+    //                 return [...prev,{productId:id,quantity:1}]
+    //             }else{
+    //                 return prev
+    //             }
+    //         }
+    //     })
+    // }
+
+    const handleQuantity = (id: number, action: "add" | "remove") => {
+        console.log(id, action);
+      
+        setCartProduct((prev)=>{
+            const existingItem = cartProduct.find((item)=>item.productId === id)
+            console.log(existingItem);
+
+            if(existingItem){
+                if(action==="add"){
+                    return prev.map((product)=>{
+                        if(product.productId === id){
+                            return {...product,quantity:product.quantity+1}
+                        }else{
+                            return product
+                        }
+                    })
+                }else if(action === 'remove'){
+                    return existingItem.quantity>1? 
+                    prev.map((product)=>product.productId ===id?{...product,quantity:product.quantity-1}:product):
+                    prev.filter((product)=> product.productId !==id)
+                }
+            }else{
+                if(action === "add"){
+                    return [...prev,{productId:id,quantity:1}]
+                }else if(action === "remove"){
+                    return prev
+                }
+            }
+            
+        })
+      };
+      
+    
+    
+    
+    console.log('after ');
+    // console.log(quantity);
   return (
     <div>
         {/* <button type="button" className="bg-indigo-500 p-4 rounded" disabled>
@@ -36,7 +115,8 @@ const Products = () => {
                 <span className="motion-safe:animate-spin">Processing...</span>
               </button> */}
         <h1 className="text-center font-bold text-xl">Products</h1>
-        <p className="text-center">Total product in Cart is {cartProduct.length}</p>
+        <p className="text-center">Total product in Cart is {cartProduct.map((item)=><p key={item.productId}>{item.productId} {item.quantity}</p>)}</p>
+         
         <div className="grid grid-cols-1 md:grid-cols-3 max-w-xl mx-auto gap-2 p-4">
             
             {products&& products.map((product:Products,index:number)=>{
@@ -48,9 +128,9 @@ const Products = () => {
                             <div className=" cursor-pointer"><FiHeart className="hover:text-red-600 hover:fill-red-600"/></div>
                             <div className=" flex justify-center gap-4 items-center">
                                 <FiShoppingCart className="fill-orange-400"/>
-                                <button className="border px-1 py-0.5 rounded hover:bg-stone-200"><FiPlus/></button>
+                                <button className="border px-1 py-0.5 rounded hover:bg-stone-200" id="addToCart"  onClick={()=>handleQuantity(product.id,"add")}><FiPlus/></button>
                                 <span>Cart </span>
-                                <button className="border px-1 py-0.5 rounded hover:bg-stone-200"><FiMinus/></button>
+                                <button className="border px-1 py-0.5 rounded hover:bg-stone-200" id="removeToCart"  onClick={()=>handleQuantity(product.id,"remove")}><FiMinus/></button>
                             </div>
                         </div>
                         <h2 className="text-wrap ">{product.title}</h2>
