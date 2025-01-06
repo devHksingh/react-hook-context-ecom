88cfb160911b8c5d665d2d173e719e9f1fc20057
devHksingh <hksingh.dev@gmail.com>
2025-01-06 21:33:44 +0530

    add index link

--- src/ErrorPage.tsx
+++ src/ErrorPage.tsx
@@ -1,15 +1,18 @@
 // import { useRouteError } from "react-router-dom"
 
+import { Link } from "react-router-dom"
+
 const ErrorPage = () => {
     // const err = useRouteError()
   return (
-    <div className="flex flex-col justify-center min-h-screen gap-4 text-xl text-center text-stone-900 bg-gray-400">
+    <div className="flex flex-col justify-center min-h-screen gap-4 text-xl text-center text-stone-900 bg-sky-100">
         <h1>Ooops!</h1>
         <p>Sorry, an unexpected error has occured.</p>
         <p>We are working on it.</p>
         <p>
             <span>{404} page not found</span>
         </p>
+        <Link  className=" max-w-sm mx-auto capitalize bg-orange-200 p-2 rounded-md shadow hover:bg-orange-400" to={''}>Back to home page</Link>
     </div>
   )
 }
