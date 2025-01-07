8ffbd392098c88b4fb6f24bdf3c6851219f15bd8
devHksingh <hksingh.dev@gmail.com>
2025-01-07 20:57:37 +0530

    add product type

---
+++ src/types/productTypes.ts
@@ -0,0 +1,13 @@
+export interface Products{
+    id:number
+    category:string
+    description:string
+    image:string
+    title:string
+    rating:{
+        rate:number
+        count:number
+    }
+    price:number
+
+}
