35d2210a229a5cf948ed95005253d1d76da2a515
devHksingh <hksingh.dev@gmail.com>
2025-01-07 20:57:25 +0530

    minor changes in routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,28 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-// import App from './App.tsx'
+
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
-import Products from './components/Products.tsx'
+
 import CartProducts from './components/CartProducts.tsx'
 import WishList from './components/WishList.tsx'
-// import Github from './components/Github.tsx'
-// import githubInfoLoader from './utils/testFetch.ts'
+
 import getAllProducts from './utils/fetchProducts.ts'
 import ErrorPage from './ErrorPage.tsx'
-import ProductPage from './components/ProductPage.tsx'
-// import githubInfoLoader from './utils/testFetch.ts'
+
+import DisplayProducts from './components/DisplayProducts.tsx'
+
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' errorElement={<ErrorPage/>}>
-      <Route path='' loader={getAllProducts} element={<Products/>}  >
-        <Route path='/:productId' element={<ProductPage/>}/>
+      <Route  path=''  element={<DisplayProducts/>}  >
+        <Route  path='/:productId' element={<DisplayProducts/>}/>
       </Route>
       
       <Route path='/cart' element={<CartProducts/>}/>
       <Route path='/wishlist' element={<WishList/>}/>
-      {/* <Route path='/git' loader={githubInfoLoader}  element={<Github/>} /> */}
+      
     </Route>
   )
 )
--- src/utils/fetchProducts.ts
+++ src/utils/fetchProducts.ts
@@ -1,30 +1,15 @@
 import axios, { AxiosError } from "axios";
 
-const getAllProducts =async()=>{
+const getAllProducts = async () => {
+
     try {
-        const products = await axios.get('https://fakestoreapi.com/products')
-        if(products){
-            console.log(products.data);
-            
-            return products.data
-        }
+        const response = await axios.get('https://fakestoreapi.com/products');
+        return response.data;
     } catch (error) {
-        if(error instanceof AxiosError){
-            console.log(error.isAxiosError)
-            console.log(error.message)
+        if (error instanceof AxiosError) {
+            throw error;
         }
     }
 }
 
 export default getAllProducts
-/*
- const githubInfoLoader = async () => {
-
-    const response = await fetch('https://api.github.com/users/hiteshchoudhary').then(data=>data.json()).then(data=>data)
-    console.log(response)
-    
-    return  response
-}
-export default githubInfoLoader
-
-*/
