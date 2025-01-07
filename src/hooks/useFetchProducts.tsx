680e804246848ac199167b7c9010b95f44be317e
devHksingh <hksingh.dev@gmail.com>
2025-01-07 20:56:26 +0530

    add custom hookfor fetch data

---
+++ src/hooks/useFetchProducts.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react"
+import { Products } from "../types/productTypes"
+import getAllProducts from "../utils/fetchProducts"
+
+const useFetchProducts = () => {
+    const [data,setData] = useState<Products[]>([])
+    const [error,setError] = useState(false)
+    const [loading,setLoading]= useState(true)
+
+    const fetchProduct =async()=>{
+        setLoading(true)
+        setError(false)
+        try {
+            const response = await getAllProducts()
+            if(response){
+                setData(response)
+            }
+        } catch (error) {
+            setError(true)
+            console.log(error)
+        }finally{
+            setLoading(false)
+        }
+    }
+    useEffect(()=>{
+        fetchProduct()
+    },[])
+  return {loading,data,error}
+}
+
+export default useFetchProducts
