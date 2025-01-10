41bac50b54f59ef78c4a6e37da9148a68b75c730
devHksingh <hksingh.dev@gmail.com>
2025-01-10 16:04:20 +0530

    optimise wishlist product

--- src/components/DisplayProducts.tsx
+++ src/components/DisplayProducts.tsx
@@ -4,58 +4,58 @@
 import { NavLink } from "react-router-dom";
 import useFetchProducts from "../hooks/useFetchProducts";
 import { FiHeart, FiMinus, FiPlus, FiShoppingCart } from "react-icons/fi";
-import { useCallback, useReducer, useState } from "react";
+import { useCallback, useReducer } from "react";
 
 const DisplayProducts = () => {
     // const navigation = useNavigation();
     const {data,error,loading:dataLoading} = useFetchProducts() ;
-    const [cartProduct,setCartProduct] = useState([{productId:0,quantity:0}])
-    const [likeProduct,setLikeProduct] = useState([{productId:0}])
-    const handleQuantity = (id: number, action: "add" | "remove") => {
-            console.log(id, action);
+    // const [cartProduct,setCartProduct] = useState([{productId:0,quantity:0}])
+    // const [likeProduct,setLikeProduct] = useState([{productId:0}])
+    // const handleQuantity = (id: number, action: "add" | "remove") => {
+    //         console.log(id, action);
           
-            setCartProduct((prev)=>{
-                const existingItem = cartProduct.find((item)=>item.productId === id)
-                console.log(existingItem);
+    //         setCartProduct((prev)=>{
+    //             const existingItem = cartProduct.find((item)=>item.productId === id)
+    //             console.log(existingItem);
     
-                if(existingItem){
-                    if(action==="add"){
-                        return prev.map((product)=>{
-                            if(product.productId === id){
-                                return {...product,quantity:product.quantity+1}
-                            }else{
-                                return product
-                            }
-                        })
-                    }else if(action === 'remove'){
-                        return existingItem.quantity>1? 
-                        prev.map((product)=>product.productId ===id?{...product,quantity:product.quantity-1}:product):
-                        prev.filter((product)=> product.productId !==id)
-                    }
-                }else{
-                    if(action === "add"){
-                        return [...prev,{productId:id,quantity:1}]
-                    }else if(action === "remove"){
-                        return prev
-                    }
-                }
+    //             if(existingItem){
+    //                 if(action==="add"){
+    //                     return prev.map((product)=>{
+    //                         if(product.productId === id){
+    //                             return {...product,quantity:product.quantity+1}
+    //                         }else{
+    //                             return product
+    //                         }
+    //                     })
+    //                 }else if(action === 'remove'){
+    //                     return existingItem.quantity>1? 
+    //                     prev.map((product)=>product.productId ===id?{...product,quantity:product.quantity-1}:product):
+    //                     prev.filter((product)=> product.productId !==id)
+    //                 }
+    //             }else{
+    //                 if(action === "add"){
+    //                     return [...prev,{productId:id,quantity:1}]
+    //                 }else if(action === "remove"){
+    //                     return prev
+    //                 }
+    //             }
                 
-            })
-          };
+    //         })
+    //       };
     
-    const handleLikeProduct = useCallback((id:number)=>{
-            console.log(id)
-            const productAlreadyExist =  likeProduct.find((product)=>product.productId === id)
-            if(productAlreadyExist){
-                setLikeProduct((prev)=>{
-                    return prev.filter((product)=> product.productId !==id)
-                })
+    // const handleLikeProduct = useCallback((id:number)=>{
+    //         console.log(id)
+    //         const productAlreadyExist =  likeProduct.find((product)=>product.productId === id)
+    //         if(productAlreadyExist){
+    //             setLikeProduct((prev)=>{
+    //                 return prev.filter((product)=> product.productId !==id)
+    //             })
                 
-            }else{
-                setLikeProduct((prev)=>[...prev,{productId:id}])
+    //         }else{
+    //             setLikeProduct((prev)=>[...prev,{productId:id}])
                 
-            }
-        },[likeProduct])
+    //         }
+    //     },[likeProduct])
 
     
     interface Action {
@@ -100,13 +100,39 @@ const DisplayProducts = () => {
 
     const [cartState,cartDispatch] = useReducer(cartReducer,initialState)
 
-    const addItemToCart = (productId:number)=>{
-        cartDispatch({type:"add",payload:{productId,quantity:1}})
+    // const addItemToCart = (productId:number)=>{
+    //     cartDispatch({type:"add",payload:{productId,quantity:1}})
+    // }
+
+    // const removeItemToCart = (productId:number)=>{
+    //     cartDispatch({type:"remove",payload:{productId,quantity:0}})
+    // }
+
+    interface WishListAction{
+        
+        payload:{
+            productId:number
+        }
+    }
+    interface WishListState{
+        productId:number;
     }
 
-    const removeItemToCart = (productId:number)=>{
-        cartDispatch({type:"remove",payload:{productId,quantity:0}})
+    function whishListReducer(wishListState:WishListState[],action:WishListAction){
+        const {payload}= action
+        const existingItem = wishListState.find((item)=>item.productId === payload.productId)
+        if(existingItem){
+            return wishListState.filter((item)=>item.productId !== payload.productId)
+        }else{
+            return [...wishListState,{productId:payload.productId}]
+        }
+        
     }
+    const [wishListState,wishListDispatch] = useReducer(whishListReducer,[{productId:0}])
+
+    const handlewishlistProduct = useCallback((productId:number)=>{
+        wishListDispatch({payload:{productId}})
+    },[])
     
     const handleAddProduct = useCallback((productId:number)=>{
         cartDispatch({type:"add",payload:{productId,quantity:1}})
@@ -114,7 +140,9 @@ const DisplayProducts = () => {
     const handleRemoveProduct = useCallback((productId:number)=>{
         cartDispatch({type:"remove",payload:{productId,quantity:0}})
     },[])
-    console.log(cartState)
+
+    
+    console.log(wishListState)
     return (
         <>
             <div className="flex-col items-center justify-center max-w-4xl p-2 mx-auto">
@@ -138,7 +166,7 @@ const DisplayProducts = () => {
                   <span>{product.id}</span>
                   <div className="p-2 mb-2"><img src={product.image} className="block aspect-square hover:rotate-1"/></div>
                   <div className="flex flex-col justify-center gap-2 m-2 place-items-center">
-                      <div className="cursor-pointer " ><FiHeart onClick={()=>handleLikeProduct(product.id)} className={`cursor-pointer ${likeProduct.find((item)=>item.productId ===product.id)?`fill-red-600 text-red-600`:`"hover:text-red-600 hover:fill-red-600"`}`} /></div>
+                      <div className="cursor-pointer " ><FiHeart onClick={()=>handlewishlistProduct(product.id)} className={`cursor-pointer ${wishListState.find((item)=>item.productId ===product.id)?`fill-red-600 text-red-600`:`"hover:text-red-600 hover:fill-red-600"`}`} /></div>
                       <div className="flex items-center justify-center gap-4 ">
                           <FiShoppingCart className="fill-orange-400"/>
                           <button className="border px-1 py-0.5 rounded hover:bg-stone-200" id="addToCart"  onClick={()=>handleAddProduct(product.id)}><FiPlus/></button>
