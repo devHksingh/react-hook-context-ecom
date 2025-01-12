2a2dc927aad4a9378e8dbfbd0100fec69f8a0686
devHksingh <hksingh.dev@gmail.com>
2025-01-12 23:48:11 +0530

    remove

--- src/contexts/cart.ts
+++ src/contexts/cart.ts
@@ -1,16 +1,16 @@
-import { createContext, useContext } from "react";
-import { CartContextType } from "../types/cartContextType";
+// import { createContext, useContext } from "react";
+// import { CartContextType } from "../types/cartContextType";
 
-export const CartContext = createContext<CartContextType|undefined>(undefined)
+// export const CartContext = createContext<CartContextType|undefined>(undefined)
 
 
-export const CartProvider = CartContext.Provider
+// export const CartProvider = CartContext.Provider
 
-export default function useCart(){
-    if( useContext(CartContext) === undefined){
-        throw new Error('cart context must have type of CartContextType')
-    }else{
-        return useContext(CartContext)
-    }
+// export default function useCart(){
+//     if( useContext(CartContext) === undefined){
+//         throw new Error('cart context must have type of CartContextType')
+//     }else{
+//         return useContext(CartContext)
+//     }
 
-}
+// }
