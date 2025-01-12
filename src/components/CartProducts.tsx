500898e5e76a300d81addee807c04c9427dda6d2
devHksingh <hksingh.dev@gmail.com>
2025-01-13 00:50:49 +0530

    add price

--- src/components/CartProducts.tsx
+++ src/components/CartProducts.tsx
@@ -14,7 +14,7 @@ const CartProducts = () => {
         <div className="flex-col items-center justify-center max-w-4xl p-2 mx-auto">
         <h1 className="text-xl font-bold text-center">CartProducts</h1>
         {cartState.map((product)=>(
-          <div className="grid max-w-4xl grid-cols-1 gap-2 p-4 mx-auto md:grid-cols-4" key={product.productId}>
+          <div className="grid max-w-6xl grid-cols-1 gap-2 p-4 mx-auto border md:grid-cols-4" key={product.productId}>
             <div className="p-2 mb-2"><img src={data.find((item)=>item.id === product.productId)?.image} className="block aspect-square hover:rotate-1"/></div>
             <div className="flex flex-col justify-center gap-1 m-2 place-items-center">
           </div>
@@ -24,6 +24,11 @@ const CartProducts = () => {
                                     <button className="border px-1 py-0.5 rounded hover:bg-stone-200" id="addToCart"  onClick={()=>handleAddProduct(product.productId)}><FiPlus/></button>
                                     <span>{cartState.find((item)=>item.productId ===product.productId)?.quantity||0} </span>
                                     <button className="border px-1 py-0.5 rounded hover:bg-stone-200" id="removeToCart"  onClick={()=>handleRemoveProduct(product.productId)}><FiMinus/></button>
+          </div>
+              
+          <div className="flex gap-4">
+            <span>Price {(data.find((item)=>item.id === product.productId)?.price as number)}</span>
+            <span>Total Price {(data.find((item)=>item.id === product.productId)?.price as number)*(cartState.find((item)=>item.productId ===product.productId)?.quantity as number)}</span>
           </div>
           
           </div>
